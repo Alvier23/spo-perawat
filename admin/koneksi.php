be8a46cb55a751233b5dedac5253b4f503bcffa4
Alvier23 <falvier@gmail.com>
2022-07-12 08:32:04 +0700

    merubah koneksi.php ke server production

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,2 +1,6 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "keperawatan");
+// dev conn
+// $koneksi = mysqli_connect("localhost", "root", "", "keperawatan");
+
+// production
+$koneksi = mysqli_connect("sql6.freemysqlhosting.net", "sql6505721", "sHE387tuGR", "sql6505721");
